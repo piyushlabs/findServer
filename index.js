ec64afa52201d2182791cbed42b0d9ea29013b4b
piyushlabs <sarin_piyush@yahoo.in>
2020-11-05 11:36:42 +0530

    Restructuring

--- index.js
+++ index.js
@@ -3,22 +3,11 @@ const urlList = require('./input.json');
 
 const urlFinder = require('./features/urlFinder');
 
-const successUrl = [];
+const successUrl = []; //list of online servers
 
-console.log("111111111111");
 
 urlFinder.findServer(urlList, successUrl).then((result) => {
-    if (successUrl.length > 0) {
-        console.log(result);
-        console.log("666666666");
-    }
-    else {
-        console.log("All serves are offline");
-    }
-}
-)
-    .catch((error) => {
+    console.log(`Most Available server is: ${result.url}, with priority: ${result.priority}`);
+    }).catch((error) => {
         console.log(error);
-    })
-
-console.log("33333333333333");
+});
